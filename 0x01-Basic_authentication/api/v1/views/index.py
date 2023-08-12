b705d70e06f1cc263963a4eaea5b465be9b1fa72
DaffiX <suleydaffy@gmail.com>
2023-08-12 18:39:59 +0300

    Unathorized entry

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,19 +26,10 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized/', strict_slashes=False)
-def unauthorized() -> None:
-    """GET /api/v1/unauthorized
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized_endpoint() -> str:
+    """ GET /api/v1/unauthorized
     Return:
-      - Unauthorized error.
+      - Raises a 401 Unauthorized error
     """
     abort(401)
-
-
-@app_views.route('/forbidden/', strict_slashes=False)
-def forbidden() -> None:
-    """GET /api/v1/forbidden
-    Return:
-      - Forbidden error.
-    """
-    abort(403)
